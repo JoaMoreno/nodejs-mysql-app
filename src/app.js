a9d617db42d6eb00c95d8366bdd6c63023b9feed
Joaquin Moreno <tec.ignaciomoreno@gmail.com>
2020-01-24 21:00:43 -0300

    express configuration

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const exphbs = require("express-handlebars");
+const path = require("path");
+
+// Initializations
+const app = express();
+require("./database")
+
+// Settings
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.engine(".hbs", exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+    helpers: require("./lib/handdlebars")
+}));
+app.set("view engine", ".hbs");
+
+// Middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended: false})); // no "img"
+
+// Global Variables
+app.use((req, res, next) =>{
+    next();
+});
+
+// Public
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use(require("./routes/routes"))
+app.use("/", require("./routes/authentication"))
+app.use("/links", require("./routes/links"))
+module.exports = app;
