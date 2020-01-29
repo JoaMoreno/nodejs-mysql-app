0d492754809ccce7644d975b4b05834dd60075a0
Joaquin Moreno <tec.ignaciomoreno@gmail.com>
2020-01-29 19:23:09 -0300

    link guide

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const router = Router();
 
+const guide = require("./guide.routes")
+
 const authentication = require("./authentication.routes")
 const links = require("./links.routes")
 
 router.use("/links",links)
 router.use("/log",authentication)
-
+router.use("/", guide)
 module.exports = router;
